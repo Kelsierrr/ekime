cafd7a23748cdcbc3a018b05c49b096cb6e4c35b
Kelsierrr <kylenni701@gmail.com>
2025-08-01 21:18:12 +0100

    small update

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Ekime Digital Services</h1>
+      <p>Digital Solutions That Drive Results.</p>
+    </div>
+  );
+}
